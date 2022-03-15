62e8172b1e6c228087642fc4873d6d40ee4934e7
KarlcxChina <self@karlcx.eu.org>
2022-03-15 11:28:18 +0800

    完成T5

--- src/Experiment3/T5.java
+++ src/Experiment3/T5.java
@@ -1,9 +1,32 @@
 package Experiment3;
 
+import java.util.Scanner;
+
 public class T5
 {
     public static void main(String[] args)
     {
-
+        while(true)
+        {
+            judge();
+        }
+    }
+    public static void judge()
+    {
+        int inp;
+        String inS = "";
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a integer: ");
+        inp = in.nextInt();
+        inS = String.valueOf(inp);
+        String rev = new StringBuilder(inS).reverse().toString();
+        if(inS.compareTo(rev) == 0)
+        {
+            System.out.printf("%s is a palindrome number\n\n",inp);
+        }
+        else
+        {
+            System.out.printf("%s is NOT a palindrome number\n\n",inp);
+        }
     }
 }
