c34be878d1211dab8b71f3ddf27d6d56df5e54d9
KarlcxChina <self@karlcx.eu.org>
2022-03-24 19:03:48 +0800

    Move Circle and CircleTest class out of main class

--- src/Experiment4/E4T1.java
+++ src/Experiment4/E4T1.java
@@ -8,41 +8,41 @@ public class E4T1
     {
         new CircleTest();
     }
-    static class Circle
+}
+class Circle
+{
+    final double PI = 3.1415926535897932384626;
+    private double radius = 0;
+    public Circle(){}
+    public Circle(double radiusIn)
     {
-        final double PI = 3.1415926535897932384626;
-        private double radius = 0;
-        public Circle(){}
-        public Circle(double radiusIn)
-        {
-            radius = radiusIn;
-        }
-        public void setRadius(double radiusIn)
-        {
-            radius = radiusIn;
-        }
-        public double getRadius()
-        {
-            return radius;
-        }
-        public double area()
-        {
-            return (PI*radius*radius);
-        }
-        public double perimeter()
-        {
-            return PI*2*radius;
-        }
+        radius = radiusIn;
     }
-    static class CircleTest
+    public void setRadius(double radiusIn)
     {
-        public CircleTest()
-        {
-            Circle circle1 = new Circle();
-            circle1.setRadius(10);
-            Circle circle2 = new Circle(5);
-            System.out.println("area:"+ circle1.area()+", perimeter:"+circle1.perimeter()+", diameter:"+circle1.getRadius()*2);
-            System.out.println("area:"+ circle2.area()+", perimeter:"+circle2.perimeter()+", diameter:"+circle2.getRadius()*2);
-        }
+        radius = radiusIn;
+    }
+    public double getRadius()
+    {
+        return radius;
+    }
+    public double area()
+    {
+        return (PI*radius*radius);
+    }
+    public double perimeter()
+    {
+        return PI*2*radius;
+    }
+}
+class CircleTest
+{
+    public CircleTest()
+    {
+        Circle circle1 = new Circle();
+        circle1.setRadius(10);
+        Circle circle2 = new Circle(5);
+        System.out.println("area:"+ circle1.area()+", perimeter:"+circle1.perimeter()+", diameter:"+circle1.getRadius()*2);
+        System.out.println("area:"+ circle2.area()+", perimeter:"+circle2.perimeter()+", diameter:"+circle2.getRadius()*2);
     }
 }
