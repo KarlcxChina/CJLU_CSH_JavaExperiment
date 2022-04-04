2850b9e7f3bd046b0748f55c5d168b538365fdf2
KarlcxChina <self@karlcx.eu.org>
2022-04-05 01:14:17 +0800

    TestGeometry is E5T2

---
+++ src/Experiment5/TestGeometry.java
@@ -0,0 +1,88 @@
+package Experiment5;
+//E5T2
+public class TestGeometry
+    {
+        public static void main(String[] args)
+            {
+                Circle c = new Circle(10);
+                System.out.println(c);
+                Cylinder cy = new Cylinder(10, 5);
+                System.out.println(cy);
+            }
+    }
+
+class Circle
+    {
+        final double PI = 3.1415926535897932384626;
+        private double radius = 0;
+
+        public Circle()
+            {
+            }
+
+        public Circle(double radiusIn)
+            {
+                radius = radiusIn;
+            }
+
+        public void setRadius(double radiusIn)
+            {
+                radius = radiusIn;
+            }
+
+        public double getRadius()
+            {
+                return radius;
+            }
+
+        public double area()
+            {
+                return (PI * radius * radius);
+            }
+
+        public double perimeter()
+            {
+                return PI * 2 * radius;
+            }
+
+        public String toString()
+            {
+                return "圆: 半径 " + getRadius() + ", 面积" + area() + ", 周长 " + perimeter();
+            }
+    }
+
+class Cylinder extends Circle
+    {
+        private double height;
+
+        public Cylinder(double r, double h)
+            {
+                setRadius(r);
+                setHeight(h);
+            }
+
+        public double getHeight()
+            {
+                return height;
+            }
+
+        public void setHeight(double height)
+            {
+                this.height = height;
+            }
+
+        public double area()
+            {
+                return 2 * super.area() + super.perimeter() * getHeight();
+            }
+
+        public double volume()
+            {
+                return super.area() * getHeight();
+            }
+
+        public String toString()
+            {
+                return "圆柱: 底半径 " + getRadius() + ", 高 " + getHeight() + ", 表面积" + area() + ", 体积 " + volume();
+            }
+    }
