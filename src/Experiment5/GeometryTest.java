30b1b320a03436578634154430d5e869dae6d28a
KarlcxChina <self@karlcx.eu.org>
2022-04-06 14:49:55 +0800

    GeometryTest is E5T4

--- src/Experiment5/GeometryTest.java
+++ src/Experiment5/GeometryTest.java
@@ -1,9 +1,41 @@
 package Experiment5;
 
 //E5T4
+
+import java.util.Scanner;
+
 public class GeometryTest
     {
-
+        public static void main(String[] args)
+            {
+                Shape shape;
+                Scanner in = new Scanner(System.in);
+                int input = 0;
+                while (true)
+                    {
+                        System.out.print("输入整数1或2(其他将退出):");
+                        input = in.nextInt();
+                        if (input == 1)
+                            {
+                                System.out.print("输入椭圆的长半轴a，短半轴b:");
+                                double a, b;
+                                a = in.nextDouble();
+                                b = in.nextDouble();
+                                shape = new Oval(a, b);
+                                System.out.println(shape);
+                            } else if (input == 2)
+                            {
+                                System.out.print("分别输入椭球的三个半轴a，b，c的长度:");
+                                double a, b, c;
+                                a = in.nextDouble();
+                                b = in.nextDouble();
+                                c = in.nextDouble();
+                                shape = new Ellipsoid(a, b, c);
+                                System.out.println(shape);
+                            } else break;
+                    }
+                System.out.println("程序运行结束！");
+            }
     }
 
 abstract class Shape
@@ -20,10 +52,17 @@ class Oval extends Shape
         private double semiMajorAxis;//半长轴
         private double semiMinorAxis;//半短轴
 
-        Oval(double a,double b)
+        Oval(double a, double b)
             {
-                this.setSemiMajorAxis(a);
-                this.setSemiMinorAxis(b);
+                if (a > b)
+                    {
+                        this.setSemiMajorAxis(a);
+                        this.setSemiMinorAxis(b);
+                    } else
+                    {
+                        this.setSemiMajorAxis(b);
+                        this.setSemiMinorAxis(a);
+                    }
             }
 
         public double getSemiMajorAxis()
@@ -55,13 +94,18 @@ class Oval extends Shape
             {
                 return 2 * PI * getSemiMinorAxis() + 4 * (getSemiMajorAxis() - getSemiMinorAxis());
             }
+
+        public String toString()
+            {
+                return "椭圆：长半轴 " + getSemiMajorAxis() + ", 短半轴 " + getSemiMinorAxis() + ", 周长 " + perimeter() + ", 面积 " + area();
+            }
     }
 
 class Ellipsoid extends Oval
     {
         private double semiSpindle;//半主轴
 
-        Ellipsoid(double a, double b,double c)
+        Ellipsoid(double a, double b, double c)
             {
                 super(a, b);
                 setSemiSpindle(c);
@@ -79,6 +123,11 @@ class Ellipsoid extends Oval
 
         public double area()
             {
-                return 0.75*PI*(getSemiSpindle()*getSemiMinorAxis() + getSemiSpindle()*getSemiMajorAxis()+getSemiMajorAxis()*getSemiMinorAxis());
+                return (4.0 / 3) * PI * (getSemiSpindle() * getSemiMinorAxis() + getSemiSpindle() * getSemiMajorAxis() + getSemiMajorAxis() * getSemiMinorAxis());
+            }
+
+        public String toString()
+            {
+                return "椭球：半轴a " + getSemiMajorAxis() + ", 半轴b " + getSemiMinorAxis() + ", 半轴c " + getSemiSpindle() + ", 表面积 " + area();
             }
     }
